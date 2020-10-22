6c59c0cd81c23de45580e6fb26021cf2890288e8
gmeben <gmeben@gmail.com>
2020-10-22 10:59:22 -0400

    build: Integrate Apollo into App components

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 
-createApp(App).mount('#app')
+Vue.use(VueApollo)
+
+const apolloClient = new ApolloClient({
+    // Use an absolute URL for the GraphQL endpoint URI
+    // uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone'
+})
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
+
+new Vue({
+    el: '#app',
+    // inject apolloProvider here like vue-router or vuex
+    apolloProvider,
+    render: h => h(App),
+})
+// createApp(App).mount('#app')
