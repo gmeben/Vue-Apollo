aa5a335785398839353b4b678ba81dfc54799b57
gmeben <gmeben@gmail.com>
2020-10-23 11:30:40 -0400

    feat: Add ApolloClient code for querying

--- client-vue-3/src/main.js
+++ client-vue-3/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 import App from './App.vue'
+
+const defaultClient = new ApolloClient({
+    uri: 'https://rickandmortyapi.com/graphql',
+    cache: new InMemoryCache
+})
+
+const query = gql`
+  query {
+    characters{
+      results {
+        name 
+      }
+    }
+  }
+`
+
+defaultClient.query({
+    query
+}).then(res => console.log(res))
 
 createApp(App).mount('#app')
