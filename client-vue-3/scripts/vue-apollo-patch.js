5b0c8685a542a40d43388c8635eb7f74031501da
gmeben <gmeben@gmail.com>
2020-10-26 10:53:13 -0400

    fix: Disable patch script

--- client-vue-3/scripts/vue-apollo-patch.js
+++ client-vue-3/scripts/vue-apollo-patch.js
@@ -1,30 +1,31 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require("fs")
+// const path = require("path")
 
-const loadTrackingPath = path.resolve(
-  __dirname,
-  '../node_modules/@vue/apollo-composable/dist/util/loadingTracking.js'
-)
+// fs.rmdirSync(path.resolve(__dirname, "../node_modules/.vite_opt_cache"), {
+//   recursive: true,
+// })
 
-fs.rmdirSync(path.resolve(__dirname, '../node_modules/.vite_opt_cache'), {
-  recursive: true
-})
+// const useQueryPath = path.resolve(
+//   __dirname,
+//   "../node_modules/@vue/apollo-composable/dist/useQuery.js"
+// )
 
-fs.writeFileSync(
-  loadTrackingPath,
-  fs.readFileSync(loadTrackingPath, 'utf8').replace(/\.\$root/m, '.root')
-)
+// const vueApolloComposablePath = path.resolve(
+//   __dirname,
+//   "../node_modules/@vue/apollo-composable/dist/vue-apollo-composable.js"
+// )
 
-const useQueryPath = path.resolve(
-  __dirname,
-  '../node_modules/@vue/apollo-composable/dist/useQuery.js'
-)
+// fs.writeFileSync(
+//   useQueryPath,
+//   fs.readFileSync(useQueryPath, "utf8").replace(/^onServerPrefetch, /mu, "")
+// )
 
-fs.writeFileSync(
-  useQueryPath,
-  fs
-    .readFileSync(useQueryPath, 'utf8')
-    .replace(/(^.*onServerPrefetch)(\.\?\.)?/m, '$1?.')
-    .replace(/(.* require\("vue"\);)/m, '')
-    .replace(/^.*(nextTick)/m, 'vue_demi_1.$1')
-)
+// fs.writeFileSync(
+//   useQueryPath,
+//   fs.readFileSync(useQueryPath, "utf8").replace(/onServerPrefetch === null.*?\}\);/mus, "")
+// )
+
+// fs.writeFileSync(
+//   vueApolloComposablePath,
+//   fs.readFileSync(vueApolloComposablePath, "utf8").replace(/vue_demi_5.onServerPrefetch === null.*?\}\);/mus, "")
+// )
