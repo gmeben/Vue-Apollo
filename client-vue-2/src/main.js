b7cabe56e95405a46b31c42eb1d72bbaf113d9cd
gmeben <gmeben@gmail.com>
2020-10-27 14:11:49 -0400

    build: Install Vue Apollo plugin into Vue

--- client-vue-2/src/main.js
+++ client-vue-2/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 
 Vue.config.productionTip = false
+Vue.use(VueApollo)
 
 const apolloClient = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql'
