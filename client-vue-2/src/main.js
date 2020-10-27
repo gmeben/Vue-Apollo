38b0a4e0383c34374bc61341cff935591ba90a82
gmeben <gmeben@gmail.com>
2020-10-27 14:10:50 -0400

    build: Add a default Apollo Client

--- client-vue-2/src/main.js
+++ client-vue-2/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ApolloClient from 'apollo-boost'
 
 Vue.config.productionTip = false
+
+const apolloClient = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql'
+})
 
 new Vue({
   render: h => h(App),
