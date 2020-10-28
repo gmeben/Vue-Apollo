ec52485ce413bbc29ea9aea49930cb02adb0b33c
gmeben <gmeben@gmail.com>
2020-10-28 11:02:18 -0400

    feat: Add buttons for pagination

--- client-vue-3/src/App.vue
+++ client-vue-3/src/App.vue
@@ -2,6 +2,11 @@
   <h1>Apollo with Vue 3</h1>
   <p>This Vue 3 app uses a publically-consumable <a href="https://rickandmortyapi.com/graphql">GraphQL API</a> from the <a href="https://rickandmortyapi.com/about">Rick and Morty API</a> as its GraphQL endpoint.</p>
   <h2>{{ message }}</h2>
+  <div>Page {{ currentPage }}</div>
+  <div>
+    <button @click="handlePage('-')">-</button>
+    <button @click="handlePage('+')">+</button>
+  </div>
   <ul class="list--characters">
     <li v-for="character in characters" :key="character.id">
       <span>{{ character.name }}</span><br/>
@@ -18,11 +23,26 @@ import allCharactersQuery from './graphql/allCharacters.query.gql'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      currentPage: 1
+    }
+  },
   setup() {
     const message = ref(`Rick and Morty Characters`)
     const { result } = useQuery(allCharactersQuery)
     const characters = useResult(result, null, data => data.characters.results)
     return { message, characters }
+  },
+  methods: {
+    handlePage: function(direction) {
+      if (direction == "+") {
+        this.currentPage++
+      }
+      if (direction == "-" && this.currentPage > 1) {
+        this.currentPage--
+      }
+    }
   }
 }
 </script>
