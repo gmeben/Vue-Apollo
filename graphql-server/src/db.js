fda2dd21d9d599db80fc6f88b9602d568eaf5e50
gmeben <gmeben@gmail.com>
2020-10-29 21:15:47 -0400

    feat: Add GraphQL server

---
+++ graphql-server/src/db.js
@@ -0,0 +1,36 @@
+export let users = [
+    { 
+        id: 1, 
+        name: "John Doe", 
+        primary_email: "john@gmail.com", 
+        age: 22,
+        datetime_created_at: "2020-10-13T20:05:02+0000",
+        datetime_locked_at: null,
+        datetime_updated_at: null,
+        is_email_verified: false,
+        is_locked: false,
+        local_password: "kLZU5EwF",
+        password_reset_token: null,
+        verify_email_token: null,
+    },
+    {
+        id: 2, 
+        name: "Jane Doe", 
+        primary_email: "jane@gmail.com", 
+        age: 23,
+        datetime_created_at: "2020-10-13T20:10:05+0000",
+        datetime_locked_at: null,
+        datetime_updated_at: null,
+        is_email_verified: false,
+        is_locked: false,
+        local_password: "yhDE3dmj",
+        password_reset_token: null,
+    },
+    {
+        id: 3, 
+        name: "Goober", 
+        primary_email: "goober@gmail.com", 
+        age: 6,
+        local_password: "aaa",
+    }
+];
